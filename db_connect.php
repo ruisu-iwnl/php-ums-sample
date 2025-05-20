6563f0cf904c934d11ded1f06b0345e6f4eca8a0
Luis Pelaez <e.luis.pelaez@gmail.com>
2025-05-20 18:03:00 +0800

    configured db

--- db_connect.php
+++ db_connect.php
@@ -1,2 +1,13 @@
 <?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'loginmanagement';
+
+$conn = mysql_connect($host, $username, $password);
+if (!$conn) {
+    die('could not connect: ' . mysql_error());
+}
+
+mysql_select_db($database, $conn);
 ?> 
